1205f218c28a88b4659fd78398380fcc2fa08d57
TMG <tom.mazogasparini@gmail.com>
2024-08-23 16:44:59 -0300

    foo

--- src/app/portfolio/assets/thumbnails/obras/index.ts
+++ src/app/portfolio/assets/thumbnails/obras/index.ts
@@ -1,15 +1,15 @@
-import obra_01 from './portfolio-2-b.jpg';
-import obra_02 from './portfolio-3.jpg';
-import obra_03 from './portfolio-8.jpg';
-import obra_04 from './portfolio-7.jpg';
-import obra_05 from './portfolio-14.jpg';
-import obra_06 from './portfolio-18.jpg';
+import obra_1 from './portfolio-2-b.jpg';
+import obra_2 from './portfolio-3.jpg';
+import obra_3 from './portfolio-8.jpg';
+import obra_4 from './portfolio-7.jpg';
+import obra_5 from './portfolio-14.jpg';
+import obra_6 from './portfolio-18.jpg';
 
 export default [
-    obra_01,
-    obra_02,
-    obra_03,
-    obra_04,
-    obra_05,
-    obra_06,
+    obra_1,
+    obra_2,
+    obra_3,
+    obra_4,
+    obra_5,
+    obra_6,
 ];
--- src/app/portfolio/assets/thumbnails/residencias/index.ts
+++ src/app/portfolio/assets/thumbnails/residencias/index.ts
@@ -1,25 +1,25 @@
-import residencia_01 from './residencia_05.jpg';
-import residencia_02 from './residencia_07.jpg';
-import residencia_03 from './residencia_08.jpg';
-import residencia_04 from './residencia_10.jpg';
-import residencia_05 from './residencia_16.jpg';
-import residencia_06 from './residencia_20.jpg';
-import residencia_07 from './residencia_22.jpg';
-import residencia_08 from './residencia_25.jpg';
-import residencia_09 from './residencia_28.jpg';
+import residencia_1 from './residencia_05.jpg';
+import residencia_2 from './residencia_07.jpg';
+import residencia_3 from './residencia_08.jpg';
+import residencia_4 from './residencia_10.jpg';
+import residencia_5 from './residencia_16.jpg';
+import residencia_6 from './residencia_20.jpg';
+import residencia_7 from './residencia_22.jpg';
+import residencia_8 from './residencia_25.jpg';
+import residencia_9 from './residencia_28.jpg';
 import residencia_10 from './residencia_30.jpg';
 import residencia_11 from './residencia_31.jpg';
 
 export default [
-    residencia_01,
-    residencia_02,
-    residencia_03,
-    residencia_04,
-    residencia_05,
-    residencia_06,
-    residencia_07,
-    residencia_08,
-    residencia_09,
+    residencia_1,
+    residencia_2,
+    residencia_3,
+    residencia_4,
+    residencia_5,
+    residencia_6,
+    residencia_7,
+    residencia_8,
+    residencia_9,
     residencia_10,
     residencia_11,
 ];
