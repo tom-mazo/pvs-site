5a610b2ed3fec0f688d45152a8c82a3b64ba3847
TMG <tom.mazogasparini@gmail.com>
2024-11-08 10:44:54 -0300

    add About Us page

---
+++ src/data/sobre-nos.ts
@@ -0,0 +1,33 @@
+import paulo from '@/assets/images/avatar_paulo.jpg'
+import tom from '@/assets/images/avatar_tom.jpg'
+import leo from '@/assets/images/avatar_leonardo.jpg'
+
+const introduction = [
+    "Há mais de 10 anos, a PVS atua na construção civil nas regiões de Sorocaba e Itu, entregando projetos de médio e alto padrão com sofisticação e qualidade. Com uma base sólida no mercado, nossa empresa é reconhecida pela excelência em cada detalhe, da fundação ao acabamento.",
+    "Agora, expandimos nossa atuação para Itaí e Paranapanema, levando nossa expertise e compromisso com a inovação para novos horizontes. Na PVS, construímos mais do que espaços; realizamos sonhos com elegância e segurança."
+]
+
+const people = [
+    {
+        name: 'Paulo Vinicius Santos',
+        title: 'Fundador | CEO',
+        description: 'Sou natural de Sorocaba, engenheiro de formação e fundador da nossa empresa. Há mais de 10 anos, atuo na região, comprometido em oferecer aos nossos clientes o mais alto padrão em construção civil. Minha missão é não apenas acompanhar, mas também liderar as novas tendências do setor, garantindo que cada projeto atenda aos mais rigorosos critérios de qualidade, inovação e satisfação.',
+        image: paulo,
+    },
+    {
+        name: 'Leonardo Gabriel Gasparini',
+        title: 'Sócio | COO',
+        description: 'Natural de Sorocaba, sou sócio e COO da empresa, com formação em Business Management and Digital Marketing na Alemanha e Cinema no Brasil. Com uma visão europeia de gerenciamento e marketing, foco em inovação e estratégia para oferecer o melhor aos nossos clientes.',
+        image: leo,
+    },
+    {
+        name: 'Tom Mazo Gasparini',
+        title: 'Sócio | CTO',
+        description: 'Como CTO e sócio da nossa empresa, trago uma perspectiva internacional ao nosso trabalho na construção civil de médio e alto padrão. Formado em Urbanismo na França e com vasta experiência como engenheiro de software na Europa, mudei-me para o Brasil há dois anos, buscando unir tecnologia e urbanismo para criar soluções inovadoras e personalizadas.',
+        image: tom,
+    }
+];
+
+const data = { introduction, people }
+
+export default data;
