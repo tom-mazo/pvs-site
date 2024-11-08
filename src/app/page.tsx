5fc2857796ec375f7881d18647c70dad03177e96
TMG <tom.mazogasparini@gmail.com>
2024-11-08 10:58:26 -0300

    add Services page; add services sections in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 import Logo from '@/assets/icons/Logo';
+import services from '@/app/servicios/data'
+import ArrowRight from '@/assets/icons/ArrowRight';
+import { Link } from "@nextui-org/react";
 import { socialMediaIcons } from "./data";
 
 
@@ -32,6 +35,25 @@ export default function Home() {
 
         </div>  
       </div>
+
+      <div className="mb-10 mt-10 rounded-lg bg-primary h-[1px] w-52 mx-auto lg:hidden"></div>
+
+      <section className="">
+        <h2 className="text-pvs-gold text-4xl font-serif mb-6">Serviços</h2>
+        <p className="text-lg max-w-xl mb-4">{'Realizamos todos os tipos de projetos residenciais e comerciais sem compromissos, adequamos valores ao perfil do clientes.'}</p>
+        <div className="pl-2 flex flex-row flex-nowrap overflow-x-scroll mb-4 -mr-4 gap-8  lg:justify-around">
+          {services.map(item => (
+            <div key={item.shortTitle} className="flex flex-row lg:flex-col items-center bg-pvs-gold rounded-lg p-4 lg:size-48 lg:justify-center lg:items-center lg:text-center cursor-pointer hover:scale-105 focus-within:scale-105 mb-2 mt-2">
+              <div className="size-24"><item.Icon color={"#04223A"}/></div>
+              <div className="text-md uppercase font-semibold text-pvs-indigo p-4">{item.shortTitle}</div>
+          </div>
+          ))}
+        </div>
+        <Link href="/servicios" className="text-pvs-gold hover:opacity-100 hover:underline hover:underline-offset-8">
+          <span className="inline-block mr-2 font-serif text-2xl">{'Veja mais'}</span>
+          <ArrowRight className="size-6" />
+        </Link>
+      </section>
 
     </div>
   );
