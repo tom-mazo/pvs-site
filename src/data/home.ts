930807f1ece5dab8d604bc84877e66bf20e360c9
TMG <tom.mazogasparini@gmail.com>
2024-11-08 12:13:37 -0300

    set up TailwindCSS

---
+++ src/data/home.ts
@@ -0,0 +1,18 @@
+import { FaInstagram, FaLinkedin, FaWhatsapp } from "react-icons/fa6";
+
+const socialMediaIcons = [
+     {
+       icon: FaInstagram,
+       link: 'https://www.instagram.com/pvsconstrutora'
+     },
+     {
+       icon: FaLinkedin,
+       link: 'https://www.linkedin.com/company/pvs-empreendimentos-imobili%C3%A1rios'
+     },
+     {
+       icon: FaWhatsapp,
+       link: 'https://wa.me/5515974019088'
+     },
+   ]
+
+export { socialMediaIcons };
